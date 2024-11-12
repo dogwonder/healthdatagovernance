bdf83fbf3caa6c2c81d491a40a85814f5d2faae3
Rich Holman <richholman@dogwonderPro.communityfibre.co.uk>
2024-11-12 12:41:58 +0000

    Add plugin

---
+++ wp-content/plugins/gp-advanced-save-and-continue/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.26',
+        'version' => '1.0.26.0',
+        'reference' => '31a00a924d15ab752e08bbbe3989c13bc7d63aef',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.26',
+            'version' => '1.0.26.0',
+            'reference' => '31a00a924d15ab752e08bbbe3989c13bc7d63aef',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
