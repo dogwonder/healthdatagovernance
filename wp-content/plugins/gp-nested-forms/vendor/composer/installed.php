f665377af274d9447fd53f371d0acf1149445146
Rich Holman <richholman@dogwonderPro.communityfibre.co.uk>
2024-11-12 11:52:39 +0000

    Multi-page nativation

---
+++ wp-content/plugins/gp-nested-forms/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.1.68',
+        'version' => '1.1.68.0',
+        'reference' => '0c1562cea467b3d78a91eee95c2a76f0a0f10e62',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.1.68',
+            'version' => '1.1.68.0',
+            'reference' => '0c1562cea467b3d78a91eee95c2a76f0a0f10e62',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
