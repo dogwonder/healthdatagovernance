0b01cb0cb56ef10c6109cb5c7425575555d3637d
Rich Holman <richholman@dogwonderPro.communityfibre.co.uk>
2024-12-17 11:05:45 +0000

    Add pagination

--- wp-content/plugins/hdg-site/src/blocks/build/cards/partials/pagination.php
+++ wp-content/plugins/hdg-site/src/blocks/build/cards/partials/pagination.php
@@ -1,74 +1,41 @@
-<?php
-$base_url = get_pagenum_link(1); 
-//If there's only one page, don't continue
-if ( $total_pages > 1 ) :
-?>
-<nav class="hdg-pagination border-top" aria-label="Pagination">
+<nav class="hdg-pagination" aria-label="Pagination">
 <ul class="hdg-pagination__list">
-	<?php
- // If current page is not the first one
- if ($paged !== 1 && $total_pages > 1): ?>
+<?php
+	// If current page is not the first one
+	if ( $paged !== 1 && $total_pages > 1 ) : ?>
 	<li class="hdg-pagination__item hdg-pagination__item--previous">
 		<a href="<?php echo $prev_url; ?>" class="hdg-pagination__link" rel="prev">
-			<svg aria-hidden="true" focusable="false" class="icon icon-arrow-left"><use xlink:href="#icon-arrow-left" /></svg>
+			<span class="hdg-pagination__link-title">
+			<svg class="hdg-pagination__link-icon" xmlns="http://www.w3.org/2000/svg" height="13" width="17" viewBox="0 0 17 13">
+			  <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
+			</svg>
+			<span class="hdg-pagination__link-text">
+			<?php esc_html_e( 'Previous page', 'hdg' ); ?>
+			</span>
+			</span>
+			<span class="visually-hidden">:</span>
+			<span class="hdg-pagination__link-label"><?php echo $paged - 1; ?> of <?php echo $total_pages; ?></span>
 		</a>
 	</li>
-	<?php endif;
- if ($total_pages > 5):
-
-   $start_page = max(2, $paged - 2);
-   $end_page = min($total_pages - 1, $paged + 2);
-   ?>
-		<li class="hdg-pagination__item">
-			<a href="<?php echo get_pagenum_link(
-     '1'
-   ); ?>" class="hdg-pagination__link">1</a>
-		</li>
-		<?php
-  if ($paged > 3) {
-    echo '<span>...</span>';
-  }
-  for ($i = $start_page; $i <= $end_page; $i++) { ?>
-			<li class="hdg-pagination__item<?php echo $paged === $i
-     ? ' hdg-pagination__item--active'
-     : ''; ?>">
-				<a href="<?php echo get_pagenum_link($i); ?>" class="hdg-pagination__link">
-					<?php echo $i; ?>
-				</a>
-			</li>
-			<?php }
-  if ($paged < $total_pages - 2) {
-    echo '<span class="hdg-pagination__seperator">...</span>';
-  }
-  ?>
-		<li class="hdg-pagination__item">
-			<a href="<?php echo get_pagenum_link(
-     $total_pages
-   ); ?>" class="hdg-pagination__link">
-				<?php echo $total_pages; ?>
-			</a>
-		</li>
-		<?php
- else:
-   for ($i = 1; $i <= $total_pages; $i++): ?>
-		<li class="hdg-pagination__item<?php echo $paged == $i
-    ? ' hdg-pagination__item--active'
-    : ''; ?>">
-			<a href="<?php echo get_pagenum_link($i); ?>" class="hdg-pagination__link">
-				<?php echo $i; ?>
-			</a>
-		</li>
-		<?php endfor;
- endif;
- // If current page is not the same as the total number of pages
- if ($paged != $total_pages): ?>
+	<?php endif; ?>
+	<?php
+	// If current page is not the same as the total number of pages
+	if ( $paged != $total_pages ) :
+		?>
 	<li class="hdg-pagination__item hdg-pagination__item--next">
 		<a href="<?php echo $next_url; ?>" class="hdg-pagination__link" rel="next">
-			<svg aria-hidden="true" focusable="false" class="icon icon-arrow-right"><use xlink:href="#icon-arrow-right" /></svg>
+			<span class="hdg-pagination__link-title">
+			<svg class="hdg-pagination__link-icon" xmlns="http://www.w3.org/2000/svg" height="13" width="17" viewBox="0 0 17 13">
+			  <path d="m10.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z"></path>
+			</svg>
+			<span class="hdg-pagination__link-text">
+			<?php esc_html_e( 'Next page', 'hdg' ); ?>
+			</span>
+			</span>
+			<span class="visually-hidden">:</span>
+			<span class="hdg-pagination__link-label"><?php echo $paged + 1; ?> of <?php echo $total_pages; ?></span>
 		</a>
 	</li>
-	<?php endif;
- ?>
+	<?php endif; ?>
 </ul>
 </nav>
-<?php endif; ?>
