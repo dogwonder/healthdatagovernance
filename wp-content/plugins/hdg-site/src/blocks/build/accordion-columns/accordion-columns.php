1bcbf0267857b7fe982d707b4641d83ac9659c14
Rich Holman <richholman@dogwonderPro.local>
2025-01-17 13:05:45 +0000

    Add arrows and PDFs

--- wp-content/plugins/hdg-site/src/blocks/build/accordion-columns/accordion-columns.php
+++ wp-content/plugins/hdg-site/src/blocks/build/accordion-columns/accordion-columns.php
@@ -41,6 +41,7 @@ $block_classes_arr = array( $class_name, $block_classes );
 			<?php
 			$accordion_column_heading = esc_html( get_sub_field( 'heading' ) ) ? : '';
 			$accordion_column_content = get_sub_field( 'content' ) ? : '';
+			$accordion_column_pdf = get_sub_field( 'pdf' ) ? : '';
 			?>
 			<div x-data="{ open: false }">
 				<div class="hdg-accordion__section" :class="{ 'open': open }">
@@ -59,14 +60,19 @@ $block_classes_arr = array( $class_name, $block_classes );
 
 						<div id="hdg-default-content-<?php echo get_row_index(); ?>" class="hdg-accordion__section-content stack" aria-labelledby="hdg-default-heading-<?php echo get_row_index(); ?>" x-on:click="open = !open" x-bind:aria-expanded="open.toString()">
 							<?php echo $accordion_column_content; ?>
+							<?php if($accordion_column_pdf) : 
+								 $accordion_column_url = $accordion_column_pdf['url'];	
+							?>
+								<p><a href="<?php echo $accordion_column_url; ?>" target="_blank" class="hdg-accordion__section-pdf hdg-button hdg-button--small">Download PDF</a></p>
+							<?php endif; ?>
 						</div>
 						
 						<div class="hdg-accordion__section-button">
 							<button class="hdg-accordion__section-button" x-on:click="open = !open" aria-controls="hdg-default-content-<?php echo get_row_index(); ?>" x-bind:aria-expanded="open.toString()" aria-label="Toggle accordion content">
 								<span class="hdg-accordion__section-button-text visually-hidden">Show/Hide</span>
 								<span class="hdg-accordion__section-button-icon" aria-hidden="true">
-									<svg class="button-open" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z"/></svg>
-									<svg class="button-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"/></svg>
+									<svg class="button-open" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM385 215c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-71-71L280 392c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-214.1-71 71c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9L239 103c9.4-9.4 24.6-9.4 33.9 0L385 215z"/></svg>
+									<svg class="button-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Pro 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2025 Fonticons, Inc.--><path d="M256 32a224 224 0 1 0 0 448 224 224 0 1 0 0-448zm0 480A256 256 0 1 1 256 0a256 256 0 1 1 0 512zM363.3 283.3l-96 96c-6.2 6.2-16.4 6.2-22.6 0l-96-96c-6.2-6.2-6.2-16.4 0-22.6s16.4-6.2 22.6 0L240 329.4 240 144c0-8.8 7.2-16 16-16s16 7.2 16 16l0 185.4 68.7-68.7c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/></svg>
 								</span>
 							</button>
 						</div>
